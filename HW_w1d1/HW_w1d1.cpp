1b73db3f75c9c050079f19cb7edfd08b01a6394c
jakelord1 <jakelord1.vo@gmail.com>
2023-09-05 17:17:02 +0300

    First 3 positions pf task

--- HW_w1d1/HW_w1d1.cpp
+++ HW_w1d1/HW_w1d1.cpp
@@ -1,15 +1,49 @@
 ﻿#include <iostream>
 using namespace std;
+
 struct Worker {
-    char* Name;
     char* SecName;
+    char* Name;
     char* Number;
     double Payment;
 };
 
+void Start() {
+    int Menu = 0;
+    cout << "Workers DataBase\nMenu:\n" << endl;
+    switch (Menu) {
+    case 1: {
 
+    }
+    case 2: {
+
+    }
+    case 3: {
+
+    }
+    case 4: {
+
+    }
+    case 5: {
+
+    }
+    case 6: {
+
+    }
+    case 7: {
+
+    }
+    case 8: {
+
+    }
+    case 0: {
+
+    }
+    }
+}
 int main()
 {
-    Worker* workers = new Worker[25];
+    Worker workers = new Worker[25];
 
+    delete[] workers;
 }
